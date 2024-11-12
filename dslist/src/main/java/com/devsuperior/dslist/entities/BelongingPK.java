419648ee3a3b260664f6ee0d1ca38427279781fb
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2024-11-12 20:01:04 -0300

    Database seed update

--- dslist/src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
+++ dslist/src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -14,7 +14,7 @@ public class BelongingPK {
     @JoinColumn(name = "game_id")
     private Game game;
     @ManyToOne
-    @JoinColumn(name = "gameList_id")
+    @JoinColumn(name = "list_id")
     private GameList list;
 
     public BelongingPK(){
